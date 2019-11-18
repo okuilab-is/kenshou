0cc557e28cf778d85315d85c033ccdf5005c9148
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-18 12:43:53 +0900

    new

---
+++ kadai2-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int i,n,stack[100],num=1,stacknum=0;
+  char ;
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+    scanf("%c",&a);
+    if(c=='('){
+      printf("%d",num);
+      stack[stacknum]=num;
+      stacknum++;
+      num++;
+    }else{
+      printf("%d",stack[stacknum-1]);
+      stacknum--;
+    }
+  }
+  printf("\n");
+  return 0;
+}
