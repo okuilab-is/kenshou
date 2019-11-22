780e838d3f9fd8e67f60bdac0834756690754582
hogehoge <ep16018@sti.chubu.ac.jp>
2019-11-22 17:51:48 +0900

    gccss

--- kadai2-1-EP16058.c
+++ kadai2-1-EP16058.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int a[100], n, b, i;
+int a, n, b, i;
 char c;
 int main(){
 	scanf("%d", &n);
 	for(i = 0; i < n; i++){
 		scanf("%c", &c);
-		if(c == '('){
-			a[b++] = c;
-		}else if(c == ')'){
-			printf("%d%d", b, b);
+		a++;
+		if(c == ')'){
+			printf("%d%d", a, a);
 		}
 	}
 	printf("\n");
+	
 
 	return 0;
 }
