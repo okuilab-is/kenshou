eab0e5acf259a3d3eb250a84dd63ea0ebca2f948
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-04 16:24:22 +0900

    gccss

---
+++ kadai1-2-EP17097.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+        int n,x,i,j,k,l=0,ans;
+        int a[13];
+        scanf("%d",&n);
+        for(i=0;i<13;i++){
+                a[i]=-1;
+        }
+        for(j=0;j<n;j++){
+                scanf("%d",&x);
+	        ans=x%13;
+        	while(ans!=a[l]){
+			ans=(x+1)%13;
+			l++;
+		}
+
+
+
+	        a[ans]=x;
+        }
+        for(k=0;k<13;k++){
+                printf("%d ",a[k]);
+        }
+        printf("\n");
+
+}
+~       
