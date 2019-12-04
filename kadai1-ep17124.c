4c9826c1fce689f907cca26c55f7a823d6f4eb19
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-04 16:03:23 +0900

    gccss

--- kadai1-ep17124.c
+++ kadai1-ep17124.c
@@ -3,14 +3,14 @@
 int main(void){
    int n, a[13], num, h, i;
 
-   scanf("%d ", &n);
+   scanf("%d", &n);
 
    for ( i = 0; i < n; i++ ) a[i] = -1;
 
    for ( i = 0; i < n; i++ ) {
       scanf("%d ", &num);
       h = num % 13;
-      while ( a[h] != -1 ) h = ( num + 1 ) % 13;
+      while ( a[h] == -1 ) h = ( num + 1 ) % 13;
       a[h] = num;
    }
 
