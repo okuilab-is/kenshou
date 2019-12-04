b3baf2058fc4375a99770fcc33bc32f4a3daf858
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-04 15:50:10 +0900

    gccss

--- kadai1-1-EP17097.c
+++ kadai1-1-EP17097.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-	int n,x,ans;
+	int n,x,i,j,ans;
 	int a[13];
 	
 	for(i=0;i<13;i++){
 		a[i]=-1;	
 	}
-	for(i=0;i<n;i++){
+	for(j=0;j<n;j++){
 		scanf("%d",&x);
 		ans=x%13;
 		a[ans]=x;	
