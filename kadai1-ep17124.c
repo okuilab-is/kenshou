ad7c4a497fb2dc6d943275c0e0c860a81e364348
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-04 16:07:01 +0900

    gccss

--- kadai1-ep17124.c
+++ kadai1-ep17124.c
@@ -5,7 +5,7 @@ int main(void){
 
    scanf("%d", &n);
 
-   for ( i = 0; i < 11111111111113; i++ ) a[i] = -1;
+   for ( i = 0; i < 13; i++ ) a[i] = -1;
 
    for ( i = 0; i < n; i++ ) {
       scanf("%d ", &num);
