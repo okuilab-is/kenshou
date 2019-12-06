2b8fe30b7340803b7af01ca3a8ef1f6390b521fc
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-06 18:09:55 +0900

    gccss

--- kadai2-EP17123.c
+++ kadai2-EP17123.c
@@ -7,12 +7,13 @@ char c;
 scanf("%d %c",&n,&c);
 
 for(i=0; i<n; i++){
-	if(c=='('){//failed 
-		x = num;
-		num++;	
+	if(c=='('){//failed
+		x=num; 
 		printf("%d",num);
+		num++;
 	}else{
 		printf("%d",x);
+		x--;
 	}
 	c++;
 }
