539955dd4aeef8ba30d84de02ce8418670478413
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-06 17:59:39 +0900

    gccss

--- kadai2-EP17123.c
+++ kadai2-EP17123.c
@@ -3,20 +3,20 @@
 int main(void){
 int n,i,num=0;
 char c;
+char *x;
 
 scanf("%d %c",&n,&c);
 
-printf("%d %d",&c,&c+1);
-
-/*
 for(i=0; i<n; i++){
 	if(c=='('){ 
 		printf("o");
 	}else{
 		printf("x");
 	}
-	*c=*c+1;
+
+	*x=&c;
+	*x+1;
+	&c=*x;
 }
-*/
 return 0;
 }
