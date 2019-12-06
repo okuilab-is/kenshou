e96c851c86c2ec73d60e9135248e281b4b0e9cc5
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-06 18:19:27 +0900

    gccss

--- kadai2-EP17123.c
+++ kadai2-EP17123.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main(void){
-int n,i,num=1,x;
+int n,i,j=0,num=1,x[100];
 char c[100];
 
 scanf("%d %s",&n,&c);
 
 for(i=0; i<n; i++){
 	if(c[i]=='('){//failed
-		x=num; 
+		x[j] = num;
 		printf("%d",num);
 		num++;
+		j++;
 	}else{
-		printf("%d",x);
-		x--;
+		printf("%d",x[j]);
+		j--;
 	}
 }
 return 0;
