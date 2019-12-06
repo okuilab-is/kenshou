f748be8b66518a5df1afd357222c7fb6527968d1
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-06 17:27:30 +0900

    gccss

---
+++ kadai1-EP17123.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+int n,h,hh,i;
+scanf("%d",&n);
+int xs[n];
+for(i=0; i<n; i++) xs[i]=-1;
+for(i=0; i<n; i++){
+	scanf("%d",&num); //入力
+	h=num%13;
+	while(xs[hh]<>0){
+		h++;
+		h=h%13;
+	}
+	xs[h]=num;
+}
+for(i=0; i<n; i++) printf("%d ",xs[i]);
+printf("\n");
+return 0;
+}
