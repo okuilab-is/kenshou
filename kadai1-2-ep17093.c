e82b8747a8bf2b3933783d32068ad74291a65eb6
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-09 11:33:54 +0900

    gccss

--- kadai1-2-ep17093.c
+++ kadai1-2-ep17093.c
@@ -1,17 +1,14 @@
 #include<stdio.h>
 void main(){
-  int n,i,a[20];
+  int n,i,a[13];
   scanf("%d",&n);
-  for (int j=0;j<20;j++) a[j]=-1;
+  for (int j=0;j<13;j++) a[j]=-1;
   int k;
   for(int j=0;j<n;j++){
     scanf("%d",&i);
     k=i%13;
-    while(a[k]!=-1) {
-      k=(k+1)%13;
-    }
+    while(a[k]!=-1) k=(k+1)%13;
     a[k]=i;
-    printf("%d,%d\n",k,a[k]);
   } 
-  for(int j=0;j<20;j++) printf("%d\n",a[j]);
+  for(int j=0;j<13;j++) printf("%d\n",a[j]);
 }
