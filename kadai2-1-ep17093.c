e069facc59be16f4d8f74e778d05bc7a6ac86e8b
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-09 11:54:09 +0900

    gccss

---
+++ kadai2-1-ep17093.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+  int n,i=1;
+  scanf("%d",&n);
+  int a[n];
+  char c;
+  for(int j=0;j<n;j++){
+    scanf("%c",&c);
+    if(c=='('){
+      a[j-(i-1)]=i;
+      i++;
+      printf("%d",a[j]);
+    }else{
+      printf("%d",i);
+    }
+  }
+  printf("\n")
+}
+
